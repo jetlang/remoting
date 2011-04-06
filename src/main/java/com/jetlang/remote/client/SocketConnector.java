b27d5c43ce6fbd27ea831792d7fc1b52c0d50781
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2011-04-06 17:52:05 +0000

    Add more events.

--- src/main/java/com/jetlang/remote/client/SocketConnector.java
+++ src/main/java/com/jetlang/remote/client/SocketConnector.java
@@ -2,13 +2,37 @@ package com.jetlang.remote.client;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  * User: mrettig
  * Date: 4/5/11
  * Time: 11:39 AM
  */
-public interface SocketConnector {
-    Socket connect() throws IOException;
+public class SocketConnector {
+
+    private final String host;
+    private final int port;
+    private final JetlangClientConfig config;
+    private boolean tcpNoDelay = true;
+
+    public SocketConnector(String host, int port, JetlangClientConfig config) {
+        this.host = host;
+        this.port = port;
+        this.config = config;
+    }
+
+    public boolean isTcpNoDelay() {
+        return tcpNoDelay;
+    }
+
+    public void setTcpNoDelay(boolean tcpNoDelay) {
+        this.tcpNoDelay = tcpNoDelay;
+    }
+
+    public Socket connect() throws IOException {
+        Socket socket = new Socket(host, port);
+        socket.setTcpNoDelay(tcpNoDelay);
+        socket.setSoTimeout(config.getHeartbeatIntervalInMs() * 2);
+        return socket;
+    }
 }
--- src/main/java/com/jetlang/remote/server/JetlangSessionConfig.java
+++ src/main/java/com/jetlang/remote/server/JetlangSessionConfig.java
@@ -3,6 +3,9 @@ package com.jetlang.remote.server;
 public class JetlangSessionConfig {
 
     private int hbIntervalInMs = 2000;
+    private boolean tcpNoDelay = true;
+    private int receiveBufferSize = 1024 * 512;
+    private int sendBufferSize = 1024 * 512;
 
     public void setHeartbeatIntervalInMs(int ms) {
         this.hbIntervalInMs = ms;
@@ -12,4 +15,27 @@ public class JetlangSessionConfig {
         return hbIntervalInMs;
     }
 
+    public boolean getTcpNoDelay() {
+        return tcpNoDelay;
+    }
+
+    public void setTcpNoDelay(boolean tcpNoDelay) {
+        this.tcpNoDelay = tcpNoDelay;
+    }
+
+    public int getReceiveBufferSize() {
+        return receiveBufferSize;
+    }
+
+    public int getSendBufferSize() {
+        return sendBufferSize;
+    }
+
+    public void setReceiveBufferSize(int receiveBufferSize) {
+        this.receiveBufferSize = receiveBufferSize;
+    }
+
+    public void setSendBufferSize(int sendBufferSize) {
+        this.sendBufferSize = sendBufferSize;
+    }
 }
