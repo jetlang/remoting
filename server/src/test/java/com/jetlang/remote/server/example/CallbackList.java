32f48b98a0a7eefbdbf4b8b4090115b06ef98d6e
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2011-04-15 02:42:30 +0000

    add req/reply

---
+++ server/src/test/java/com/jetlang/remote/server/example/CallbackList.java
@@ -0,0 +1,20 @@
+package com.jetlang.remote.server.example;
+
+import org.jetlang.core.Callback;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CallbackList<T> implements Callback<T> {
+
+    public final List<T> received = Collections.synchronizedList(new ArrayList<T>());
+
+    public void onMessage(T t) {
+        received.add(t);
+    }
+
+    public static <T> CallbackList<T> create() {
+        return new CallbackList<T>();
+    }
+}
