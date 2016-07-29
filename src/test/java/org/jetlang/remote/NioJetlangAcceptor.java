4d1f6e49b46ba606db8f5033623f919c84a35fff
Mike Rettig <mrettig@drw.com>
2016-07-29 09:24:34 -0500

    jdk 8 only

---
+++ src/test/java/org/jetlang/remote/NioJetlangAcceptor.java
@@ -0,0 +1,124 @@
+package org.jetlang.remote;
+
+import org.jetlang.fibers.NioBatchExecutorImpl;
+import org.jetlang.fibers.NioChannelHandler;
+import org.jetlang.fibers.NioControls;
+import org.jetlang.fibers.NioFiber;
+import org.jetlang.fibers.NioFiberImpl;
+import org.jetlang.remote.acceptor.NioAcceptorHandler;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Collections;
+import java.util.List;
+
+public class NioJetlangAcceptor {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final List<NioChannelHandler> objects = Collections.emptyList();
+        NioFiberImpl.WriteFailure writeFailure = new NioFiberImpl.WriteFailure() {
+            @Override
+            public <T extends SelectableChannel & WritableByteChannel> void onFailure(IOException e, T t, ByteBuffer byteBuffer) {
+                e.printStackTrace();
+            }
+        };
+        final NioFiberImpl fiber = new NioFiberImpl(new NioBatchExecutorImpl(), objects, "nioThread", false, writeFailure, new NioFiberImpl.NoOpBuffer());
+        final NioAcceptorHandler acceptorEnd = NioAcceptorHandler.create(8089,
+                (acceptorFiber, controls, key, channel) -> {
+                    controls.addHandler(new Client(channel, fiber));
+                }, () -> System.out.println("AcceptorEnd")
+        );
+        fiber.addHandler(acceptorEnd);
+        fiber.start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                System.out.println("Disposing");
+                fiber.dispose();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+
+                }
+                System.out.println("Disposed");
+
+            }
+        });
+        System.out.println("Done");
+    }
+
+    private static class Client implements NioChannelHandler {
+        private final SocketChannel accept;
+        private final NioFiberImpl nioWriter;
+        private final byte[] data = new byte[1024 * 1024];
+        final ByteBuffer allocate = ByteBuffer.wrap(data);
+        private String remoteAddress;
+
+        public Client(SocketChannel accept, NioFiberImpl nioWriter) {
+            this.accept = accept;
+            this.nioWriter = nioWriter;
+            try {
+                accept.socket().setSendBufferSize(8);
+                accept.socket().setReceiveBufferSize(1024 * 1024);
+                accept.socket().setTcpNoDelay(true);
+                System.out.println(accept.socket().getSendBufferSize());
+                this.remoteAddress = accept.getRemoteAddress().toString();
+            } catch (IOException e) {
+                this.remoteAddress = "unknown";
+            }
+            System.out.println("Connected: " + remoteAddress);
+        }
+
+        @Override
+        public boolean onSelect(NioFiber nioFiber, NioControls controls, SelectionKey key) {
+            try {
+                int read = accept.read(allocate);
+                if (read == -1) {
+                    return false;
+                }
+                System.out.println("read = " + read);
+                allocate.flip();
+                controls.write(accept, allocate);
+                allocate.clear();
+                return true;
+            } catch (IOException e) {
+                return false;
+            }
+        }
+
+        @Override
+        public SelectableChannel getChannel() {
+            return accept;
+        }
+
+        @Override
+        public int getInterestSet() {
+            return SelectionKey.OP_READ;
+        }
+
+        @Override
+        public void onEnd() {
+            try {
+                accept.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Client end: " + remoteAddress);
+        }
+
+        @Override
+        public void onSelectorEnd() {
+            try {
+                accept.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("SelectorEnd ClientEnd: " + remoteAddress);
+        }
+    }
+}
---
+++ src/test/java/org/jetlang/remote/NioJetlangClient.java
@@ -0,0 +1,65 @@
+package org.jetlang.remote;
+
+import org.jetlang.fibers.NioBatchExecutorImpl;
+import org.jetlang.fibers.NioControls;
+import org.jetlang.fibers.NioFiber;
+import org.jetlang.fibers.NioFiberImpl;
+import org.jetlang.remote.acceptor.NioClientHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class NioJetlangClient {
+
+    private static byte val = (byte) '1';
+
+
+    public static void main(String[] args) throws IOException {
+        NioFiberImpl fiber = new NioFiberImpl(new NioBatchExecutorImpl(), Collections.emptyList(), "client", false, new NioFiberImpl.NoOpWriteFailure(), new NioFiberImpl.NoOpBuffer());
+        fiber.start();
+        NioClientHandler handler = NioClientHandler.create("localhost", 8089, new NioClientHandler.Reader() {
+            byte[] bytes = new byte[1024 * 1024];
+            ByteBuffer buffer = ByteBuffer.wrap(bytes);
+            int totalRead;
+
+            @Override
+            public boolean onRead(NioFiber nioFiber, NioControls controls, SelectionKey key, SocketChannel channel) {
+                try {
+                    int read = channel.read(buffer);
+                    if (read == -1) {
+                        System.out.println("endread = " + read);
+                        return false;
+                    }
+                    totalRead += read;
+                    buffer.flip();
+                    System.out.println("read = " + read + " " + totalRead);
+                    for (int i = 0; i < read; i++) {
+                        if (val != buffer.get()) {
+                            System.out.println(val);
+                        }
+                    }
+                    //System.out.println(new String(bytes, 0, read));
+                    buffer.clear();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return true;
+            }
+        });
+        fiber.addHandler(handler);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (String line = reader.readLine(); line.length() > 0; line = reader.readLine()) {
+            int length = Integer.parseInt(line);
+            byte[] toSend = new byte[length];
+            Arrays.fill(toSend, val);
+            fiber.execute((controls) -> controls.write(handler.getSocket(), ByteBuffer.wrap(toSend)));
+        }
+        fiber.dispose();
+    }
+}
