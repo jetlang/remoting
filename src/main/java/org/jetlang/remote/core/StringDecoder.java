d112d206a222dacbf6e989279d07815adf696718
Mike Rettig <mrettig@drwholdings.com>
2023-08-28 12:14:01 -0500

    remove illegal access.

--- src/main/java/org/jetlang/remote/core/StringDecoder.java
+++ src/main/java/org/jetlang/remote/core/StringDecoder.java
@@ -1,10 +1,7 @@
 package org.jetlang.remote.core;
 
-import sun.nio.cs.ArrayDecoder;
-
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
 
 public interface StringDecoder {
 
@@ -38,24 +35,7 @@ public interface StringDecoder {
     }
 
     static ByteArrayDecoder createByteArrayDecoder(Charset cs){
-        CharsetDecoder charsetDecoder = cs.newDecoder();
-        if (charsetDecoder instanceof ArrayDecoder) {
-            return new ByteArrayDecoder() {
-                ArrayDecoder decoder = (ArrayDecoder) charsetDecoder;
-
-                @Override
-                public String decode(byte[] bytes, int offset, int length) {
-                    char[] cs = new char[(int) (length * (double) charsetDecoder.maxCharsPerByte())];
-                    int result = decoder.decode(bytes, offset, length, cs);
-                    if (result == -1) {
-                        return null;
-                    }
-                    return new String(cs, 0, result);
-                }
-            };
-        } else {
-            return (bytes, offset, length) -> new String(bytes, offset, length, cs);
-        }
+        return (bytes, offset, length) -> new String(bytes, offset, length, cs);
     }
 
 }
